7b9f48d430e17a10b8c09f4e19505ea74ecdb857
Zhicheng WANG <zcwang@thoughtworks.com>
2021-11-05 17:45:33 +0800

    chore: publish to sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,14 @@ val kotlin_version = "1.5.31"
 plugins {
     kotlin("jvm") version "1.5.31"
     `maven-publish`
+    signing
 }
 group = "wang.ralph.common"
 version = "2.1.0"
 
 repositories {
+    mavenLocal()
     mavenCentral()
-}
-
-java {
-    withSourcesJar()
 }
 
 dependencies {
@@ -35,6 +33,64 @@ publishing {
             artifactId = project.name
             version = project.version.toString()
             from(components["java"])
+            sourceSets {
+                main {
+                    java {
+                        srcDirs("src/main/kotlin")
+                    }
+                }
+            }
+        }
+        withType<MavenPublication> {
+            pom {
+                name.set("${project.group}:${project.name}")
+                url.set("https://github.com/asnowwolf/ktor-graphql")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                organization {
+                    name.set("Ralph WANG")
+                    name.set("https://blog.ralph.wang/")
+                }
+                developers {
+                    developer {
+                        name.set("Ralph WANG")
+                        email.set("asnowwolf@gmail.com")
+                        organization.set("Ralph WANG")
+                        organizationUrl.set("https://blog.ralph.wang/")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/asnowwolf/ktor-graphql.git")
+                    developerConnection.set("scm:git:git://github.com/asnowwolf/ktor-graphql.git")
+                    url.set("https://github.com/asnowwolf/ktor-graphql")
+                }
+
+                // child projects need to be evaluated before their description can be read
+                val mavenPom = this
+                afterEvaluate {
+                    mavenPom.description.set(project.description)
+                }
+            }
         }
     }
+    repositories {
+        maven {
+            url = uri(project.property("sonatype.release-url") as String)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = project.property("sonatype.username") as String
+                password = project.property("sonatype.password") as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(configurations.archives.get())
 }
